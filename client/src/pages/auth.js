b4093f234ed1e6bf45ffdcea5243598e6f9d7d60
miguelmanasdev <miguel.manas.alvarez.dev@gmail.com>
2025-03-27 23:46:54 +0100

    MERN Recipe App with Authentication - Build & Deploy A React Intermediate Project p3

--- client/src/pages/auth.js
+++ client/src/pages/auth.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 export const Auth = () => {
     return <div className="auth">
         <Login />
@@ -10,5 +12,28 @@ const Login = () => {
 };
 
 const Register = () => {
-    return <div> </div>
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+
+    return <div className="auth-container">
+        <form>
+            <h2>Register</h2>
+            <div className="form-group">
+                <label htmlFor="username"> Username: </label>
+                <input 
+                type="text" 
+                id="username" 
+                value={username} 
+                onChange={(event) => setUsername(event.target.value)}></input>
+            </div>
+            <div className="form-group">
+                <label htmlFor="password"> Password: </label>
+                <input 
+                type="text" 
+                id="password" 
+                value={password} 
+                onChange={(event) => setPassword(event.target.value)}></input>
+            </div>
+        </form>
+     </div>
 };
